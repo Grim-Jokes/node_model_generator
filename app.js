3c016424c040cff09bd0a8b646353ffa628a6f9b
Grim Jokes <danszekely@gmail.com>
2015-11-09 21:35:12 -0500

    Updated -S command description

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ for (i = 2; i < process.argv.length; i++) {
 	if (process.argv[i] == '-H' || process.argv[i] == '-h') {
 		
 		console.info("-C -- The database connection string");
-		console.info("-S -- The database schema to use in postgres");
+		console.info("-S -- The database schema to use in postgres. Defaults to database name");
 		console.info("-P -- The path in which to create model files.");
 
 		process.exit();	
