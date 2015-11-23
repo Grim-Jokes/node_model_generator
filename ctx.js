f10124058b996c29e631cf24912be0528dae4ee8
Grim-Jokes <danszekely@gmail.com>
2015-11-23 05:56:38 -0800

    Add basic retrieval logic from the db

--- ctx.js
+++ ctx.js
@@ -1,18 +1,20 @@
 //AUTO-GENERATED FILE: Do not update code here or it could be overwritten if the tool is ran again.
-//Generated On: 2015-11-22 14:29:22.325Z
+//Generated On: 2015-11-23 13:23:43.524Z
 
-var pg = require("pg");
+var _super = require("./lib/postgres/context");
 var collection = require("./lib/postgres/collection");
 
-var ctx = DbContext.prototype;
+var ctx = DbContext.prototype = Object.create(_super.prototype);
+ctx.constructor = DbContext;
 
 function DbContext(conString) {
-	this._constring = conString;
+	_super.call(this, conString);
 }
 
 Object.defineProperty(ctx, "transactions", {get: function () {
 		if (!this._transactions) {
 			this._transactions = new collection("transaction");
+			this._transactions.context = this;
 		}
 
 		return this._transactions;
@@ -22,6 +24,7 @@ Object.defineProperty(ctx, "transactions", {get: function () {
 Object.defineProperty(ctx, "persons", {get: function () {
 		if (!this._persons) {
 			this._persons = new collection("person");
+			this._persons.context = this;
 		}
 
 		return this._persons;
--- test.js
+++ test.js
@@ -4,8 +4,21 @@ var DbContext = require("./ctx.js")
 var ctx = new DbContext(process.argv[3])
 console.log(ctx.persons.query)
 
+var d = { d : "Daniel", l : "s" };
 
+var q = ctx.persons.where(ex=> first_name == "{d}" || last_name == "{l}", d);
 
+console.log(q.query);
+
+var q = ctx.persons.where(ex=> first_name == "Daniel");
+var q2 = q.where(ex=> last_name == "Szekely")
+console.log(q.query);
+console.log(q2.query);
+
+var result = q.to_list(function (rows) {
+    console.log(rows);
+    return;
+});
 
 
 return;
